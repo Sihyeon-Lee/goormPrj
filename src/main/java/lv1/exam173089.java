d6c0eb2237599fd79e6898d4ab317aba8c79d8c7
leesihyeon <pubu0880@naver.com>
2024-11-08 09:08:52 +0900

    24-11-08 정수의 길이

--- src/main/java/lv1/exam173089.java
+++ src/main/java/lv1/exam173089.java
@@ -3,6 +3,7 @@ package lv1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+// 정수의 길이
 public class exam173089 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
