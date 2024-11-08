a48dc25558649db740b50baba18fb0eec409341a
leesihyeon <pubu0880@naver.com>
2024-11-08 09:08:18 +0900

    24-11-08

---
+++ src/main/java/lv1/exam173089.java
@@ -0,0 +1,12 @@
+package lv1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class exam173089 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input.length());
+    }
+}
